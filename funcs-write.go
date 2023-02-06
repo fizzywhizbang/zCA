b9c6caf8fe90808f907495f06b50677700c0e23f
FizzyWhizbang <marc.levine@gmail.com>
2023-02-06 15:46:06 -0600

    updated template form intermediate
    updated template for intermediate and started adding a non-repudiation cert

--- funcs-write.go
+++ funcs-write.go
@@ -140,7 +140,8 @@ func intermediate(iss *issuer, cn string, y int, C, S, L, O, OU []string, config
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
 		BasicConstraintsValid: true,
-		IsCA:                  false,
+		IsCA:                  true,
+		MaxPathLenZero:        true,
 	}
 	der, err := x509.CreateCertificate(rand.Reader, template, iss.cert, key.Public(), iss.key)
 	if err != nil {
@@ -197,6 +198,64 @@ func sign(iss *issuer, cn string, y int, domains, ipAddresses, C, S, L, O, OU []
 
 		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageKeyEncipherment,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth, x509.ExtKeyUsageClientAuth},
+		BasicConstraintsValid: true,
+		IsCA:                  false,
+	}
+	der, err := x509.CreateCertificate(rand.Reader, template, iss.cert, key.Public(), iss.key)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.OpenFile(fmt.Sprintf("%s/%scert.pem", cnFolder, cn), os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0600)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	err = pem.Encode(file, &pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: der,
+	})
+	if err != nil {
+		return nil, err
+	}
+	return x509.ParseCertificate(der)
+}
+
+func userCertificate(iss *issuer, cn string, y int, domains, ipAddresses, C, S, L, O, OU []string, config ZcaConfig) (*x509.Certificate, error) {
+	var cnFolder = config.CertDir + "/" + strings.Replace(cn, "*", "_", -1)
+	err := os.Mkdir(cnFolder, 0700)
+	if err != nil && !os.IsExist(err) {
+		return nil, err
+	}
+	key, err := makeKey(fmt.Sprintf("%s/%s-key.pem", cnFolder, cn))
+	if err != nil {
+		return nil, err
+	}
+	parsedIPs, err := parseIPs(ipAddresses)
+	if err != nil {
+		return nil, err
+	}
+	serial, err := rand.Int(rand.Reader, big.NewInt(math.MaxInt64))
+	if err != nil {
+		return nil, err
+	}
+	template := &x509.Certificate{
+		DNSNames:    domains,
+		IPAddresses: parsedIPs,
+		Subject: pkix.Name{
+			Country:            C,
+			Province:           S,
+			Locality:           L,
+			Organization:       O,
+			OrganizationalUnit: OU,
+			CommonName:         cn,
+		},
+		SerialNumber: serial,
+		NotBefore:    time.Now(),
+		NotAfter:     time.Now().AddDate(y, 0, 0),
+
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageContentCommitment,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageEmailProtection, x509.ExtKeyUsageCodeSigning},
 		BasicConstraintsValid: true,
 		IsCA:                  false,
 	}
